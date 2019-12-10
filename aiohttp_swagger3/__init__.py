e8296795952e020af5b5504a53f0a187b54624a5
Konstantin Valetov <Konstantin_Valetov@epam.com>
2019-12-10 21:27:00 +0300

    0.3.0

--- aiohttp_swagger3/__init__.py
+++ aiohttp_swagger3/__init__.py
@@ -1,5 +1,5 @@
 __all__ = ("SwaggerDocs", "SwaggerFile", "SwaggerUiSettings", "__version__")
-__version__ = "0.2.5"
+__version__ = "0.3.0"
 __author__ = "Valetov Konstantin"
 
 from .swagger_docs import SwaggerDocs
