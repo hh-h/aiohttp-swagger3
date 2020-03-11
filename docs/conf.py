c5985ca0a6ddfa2506defb292e35102417330fc9
Konstantin Valetov <Konstantin_Valetov@epam.com>
2020-03-11 19:25:04 +0300

    fix documentation

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Konstantin Valetov'
 author = 'Konstantin Valetov'
 
 # The full version, including alpha/beta/rc tags
-release = '0.4.0'
+release = '0.4.2'
 
 
 # -- General configuration ---------------------------------------------------
