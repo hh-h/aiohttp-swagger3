aaf910af8c780b1e02d887ee6d69d3f0d21716cc
Konstantin Valetov <Konstantin_Valetov@epam.com>
2020-12-31 12:46:39 +0300

    added required readOnly properties support

--- tests/test_docs_request_bodies.py
+++ tests/test_docs_request_bodies.py
@@ -1437,3 +1437,72 @@ async def test_object_read_only_properties_passed(swagger_docs, aiohttp_client):
             "object": msg,
         }
     }
+
+
+async def test_object_required_read_only_properties(swagger_docs, aiohttp_client):
+
+    routes = web.RouteTableDef()
+
+    @routes.post("/r")
+    async def handler(request, body: Dict):
+        """
+        ---
+        requestBody:
+          required: true
+          content:
+            application/json:
+              schema:
+                type: object
+                required:
+                  - id
+                  - name
+                  - description
+                  - updated_at
+                properties:
+                  id:
+                    type: integer
+                    readOnly: true
+                  name:
+                    type: string
+                  description:
+                    type: string
+                  updated_at:
+                    type: integer
+                    readOnly: true
+
+        responses:
+          '200':
+            description: OK.
+        """
+        return web.json_response(body)
+
+    swagger = swagger_docs()
+    swagger.add_routes(routes)
+
+    client = await aiohttp_client(swagger._app)
+
+    body = {
+        "name": "name1",
+        "description": "description",
+    }
+    resp = await client.post("/r", json=body)
+    assert resp.status == 200
+    assert await resp.json() == body
+
+    resp = await client.post(
+        "/r",
+        json={
+            "id": 1,
+            "name": "name1",
+            "description": "description",
+            "updated_at": 1609407277,
+        },
+    )
+    assert resp.status == 400
+    error = error_to_json(await resp.text())
+    assert error == {
+        "body": {
+            "id": "property is read-only",
+            "updated_at": "property is read-only",
+        }
+    }
