2ded685549310bff7f7ecaafc01849bbf446586e
hh-h <666@thetrue.name>
2023-02-04 13:00:36 +0400

    0.7.4

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2023, Konstantin Valetov'
 author = 'Konstantin Valetov'
 
 # The full version, including alpha/beta/rc tags
-release = '0.7.3'
+release = '0.7.4'
 
 
 # -- General configuration ---------------------------------------------------
