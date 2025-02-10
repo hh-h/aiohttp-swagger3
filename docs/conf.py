a63a657373b4942376a1d764f57211261eceb06a
Leo Smerling <leosmerling@noreply.github.com>
2025-02-10 11:40:22 +0100

    chore: pin aiotthp version to <4 to allow 3.11 and future 3.x aiohttp updates

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2023, Konstantin Valetov'
 author = 'Konstantin Valetov'
 
 # The full version, including alpha/beta/rc tags
-release = '0.9.0'
+release = '0.10.0'
 
 
 # -- General configuration ---------------------------------------------------
